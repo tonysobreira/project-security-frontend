97ceef33a4303c6ac81f2082114856aa46511b0e
Tony <tonysobreira@gmail.com>
2023-11-14 16:05:48 -0300

    Update middleware.ts
    build correction

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getSession } from 'next-auth/react';
+//import { getSession } from 'next-auth/react';
+import { getToken } from 'next-auth/jwt';
 
 export const config = {
     matcher: ['/', '/blocks/:path*', '/documentation/:path*', '/pages/:path*', '/uikit/:path*', '/utilities/:path*']
@@ -14,7 +15,8 @@ export default async function middleware(req: NextRequest, res: NextResponse) {
         }
     };
 
-    const session = await getSession({ req: requestForNextAuth });
+    //const session = await getSession({ req: requestForNextAuth });
+    const session = await getToken({ req: req, secret: process.env.NEXTAUTH_SECRET });
 
     console.log(session);
 
